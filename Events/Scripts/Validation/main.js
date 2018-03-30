82c63596bfa6fb83b76df1e1b34edb357d84cffe
Julio Vasquez <30414199+DarKPhuRioN@users.noreply.github.com>
2018-03-30 14:36:30 -0500

    Update main validation js

--- Events/Scripts/Validation/main.js
+++ Events/Scripts/Validation/main.js
@@ -1,36 +1,50 @@
-(function($) {
-    "use strict";
-    /*==================================================================
-    [ Validate ]*/
-    /*input con esas 2 clases*/
-    var input = $('.validate-input .input100');
-    $('.validate-form').on('submit', function() {
-        var check = true;
-        for (var i = 0; i < input.length; i++) {
-            if (!validate(input[i])) {
-                showValidate(input[i]);
-                check = false;
-            }
+var input = document.getElementsByClassName('input100');
+let x = false;
+document.getElementById('btn').addEventListener('click', () => {
+    var temp = 0;
+    for (var i = 0; i < input.length; i++) {
+        if (validate(input[i])) {
+            ShowClass(input[i].parentNode);
+            temp++;
         }
-        return check;
-    });
-    //form input
-    $('.validate-form .input100').each(function() {
-        $(this).focus(function() {
-            hideValidate(this);
+    }
+    x = (temp == 0) ? true : false;
+});
+
+window.onload = () => {
+    for (var i = 0; i < input.length; i++) {
+        console.log(i);
+        let dad = input[i].parentNode;
+        input[i].addEventListener('focus', () => {
+            HideClass(dad);
         });
-    });
-    function validate(input) {//validaciones
-        if ($(input).val().trim() == '') {
-            return false;
-        }
     }
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).addClass('alert-validate');
+}
+
+function validate(input) {
+    return (input.value.trim() == '');
+}
+
+function ShowClass(dad) {//si no la tiene pongala
+    if (!Verificate(dad, 'alert-validate')) {
+        dad.classList.add('alert-validate');
     }
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).removeClass('alert-validate');
+}
+
+function HideClass(dad) {//si la tiene quitela
+    if (!Verificate(dad, 'alert-validate')) {
+        dad.classList.remove('alert-validate');
     }
-})(jQuery);
+}
+
+function val() {//todo pasa por aqui
+    return x;
+}
+
+function Verificate(dad, clas) {
+    let res = 0;
+    for (var i = 0; i < dad.classList.length; i++) {
+        res += (dad.classList[i] == clas) ? 1 : 0;
+    }
+    return (res == dad.classList.length);
+}
